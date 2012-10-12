9fbff0b66e29e9e10801a509e874fd2f999d4855
Tommy Gudmundsen <tommy@funkbit.no>
2012-10-12 16:30:50 +0200

    Moved import with dependency on celery.

--- analytics_client/__init__.py
+++ analytics_client/__init__.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from analytics_client.client import Metric
-from analytics_client.tasks import store_metric
 
 version_info = (0, 1, 2)
 __version__ = '.'.join(map(str, version_info))
@@ -15,6 +14,8 @@ def save_metric(key, value, timestamp=None):
     """
     Wrapper used to save a metric using a celery task.
     """
+
+    from analytics_client.tasks import store_metric
 
     # Set a timestamp if it is undefined
     _timestamp = timestamp
