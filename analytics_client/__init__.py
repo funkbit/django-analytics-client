ceecb1058bdae02a5f72d072bd96959760181287
Tommy Gudmundsen <tommy@funkbit.no>
2012-10-12 16:24:13 +0200

    Added wrapper function for storing a metric using a celery task.

--- analytics_client/__init__.py
+++ analytics_client/__init__.py
@@ -1,2 +1,24 @@
-version_info = (0, 1, 1)
+from datetime import datetime
+
+from analytics_client.client import Metric
+from analytics_client.tasks import store_metric
+
+version_info = (0, 1, 2)
 __version__ = '.'.join(map(str, version_info))
+
+
+#####################
+# Utility functions #
+#####################
+
+def save_metric(key, value, timestamp=None):
+    """
+    Wrapper used to save a metric using a celery task.
+    """
+
+    # Set a timestamp if it is undefined
+    _timestamp = timestamp
+    if _timestamp is None:
+        _timestamp = datetime.now()
+
+    store_metric.delay(Metric(key=key, value=value, timestamp=_timestamp))
